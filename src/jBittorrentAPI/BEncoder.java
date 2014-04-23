5f33cd2fd564051c313245612dac67791644a751
dan-shaffer <shaffer2112@gmail.com>
2014-04-22 23:38:52 -0400

    spelling

--- src/jBittorrentAPI/BEncoder.java
+++ src/jBittorrentAPI/BEncoder.java
@@ -28,7 +28,7 @@ import java.util.*;
 
 /**
  * A set of utility methods to encode a Map into a bencoded array of byte.
- * integer are represented as Long, String as byte[], dictionnaries as Map, and list as List.
+ * integer are represented as Long, String as byte[], dictionaries as Map, and list as List.
  *
  * @author  TdC_VgA
  */
